28d8ec199e5854fa7044d037c60fab0416ce7284
Abdullah Mersinlioglu <a.mersin61300@yahoo.com>
2020-04-01 00:58:57 +0300

    Initial commit

---
+++ e-Commerce Admin/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  e-Commerce Admin
+//
+//  Created by Abdullah Mersinliogl on 1.04.2020.
+//  Copyright © 2020 Abdullah Mersinliogl. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
